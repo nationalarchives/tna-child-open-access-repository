b4474c0f1f4fcaf16b1efdb168249546c8f62082
Punal Chotrani <punalsc@gmail.com>
2015-12-09 15:24:18 +0000

    Added dynamic breadcrumbs, research-details template, added custom fields just for child theme.

--- research-deatils-template.php
+++ research-deatils-template.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pchotrani
- * Date: 07/12/15
- * Time: 16:12
- */
-
 /*
 Template Name: OA Research details template
 */
@@ -14,14 +7,14 @@ get_header();
 
 ?>
 <div class="container" id="page_wrap" role="main">
-    <?php include 'includes/breadcrumb.php'; ?>
+    <?php include 'breadcrumb.php'; ?>
 <div class="row">
     <div class="col starts-at-full ends-at-two-thirds box clr">
         <div class="heading-holding-banner">
             <h1>
                 <span>
                     <span>
-                       Registered Designs at The National Archives
+                       <?php echo get_the_title(); ?>
                     </span>
                 </span>
             </h1>
@@ -30,9 +23,9 @@ get_header();
             <span class="entry-meta"><strong>Author(s):</strong></span>
             <span class="entry-meta">Julie Halls</span>
             <div class="clearfix"></div>
-            <span class="entry-meta"><strong>Date of publication:</strong> 25/11/2015</span>
+            <span class="entry-meta"><strong>Date of publication:</strong> <?php the_date('d/m/y'); ?></span>
             <br />
-            <span class="entry-meta"><strong>Published by:</strong> Laura Ipsum</span>
+            <!--<span class="entry-meta"><strong>Published by:</strong> Laura Ipsum</span>-->
             <hr />
             <div class="clearfix"></div>
             <span class="entry-meta"><strong>Keywords:</strong> </span>
@@ -41,12 +34,9 @@ get_header();
             <span class="entry-meta">material culture</span>
             <hr />
             <div class="clearfix"></div>
-            <p>
-                <strong>Abstract:</strong> The registered designs held at The National Archives, the UK government’s archive in Kew, South West London, form a rich and unique resource for studying design history that has yet to be fully explored. Looking at designs registered for copyright between 1842 and 1883, in particular those relating to interior design—including wallpapers, furnishing textiles and ceramics—this article shows how the material, including many thousands of drawings, photographs and samples, fits within the wider context of the nineteenth-century design reform movement.
-            </p>
-            <p>
-                Debates around the quality of design, public taste and the education of designers, which resulted in the setting up of the Schools of Design and the South Kensington Museum, also led to the series of design copyright acts that gave rise to this collection. The article looks at the difficulties involved in attributing the designs and the position of anonymous designers working for big manufacturers, and gives examples of The National Archives’ holdings of wallpaper, textile, ceramic and furniture designs.
-            </p>
+            <?php
+                the_content();
+            ?>
             <div class="clearfix"></div>
             <a class="button float-right" href="#">Download PDF (123KB)</a>
         </div>
