ab8f36ec9132bbb03e98540c4142fa5ecc064e1e
Punal Chotrani <punalsc@gmail.com>
2015-12-14 10:54:13 +0000

    Added side bar

--- research-deatils-template.php
+++ research-deatils-template.php
@@ -82,24 +82,9 @@ get_header();
         </div>
         <!--  Research ends here-->
     </div>
-    <div class="col starts-at-full ends-at-one-third clr box">
-        <div class="heading-holding-banner">
-            <h2>
-                <span>
-                    <span>
-                        You may also be interested in
-                    </span>
-                </span>
-            </h2>
-        </div>
-        <div class="breather">
-            <ul class="sibling">
-                <li>Lorem ipsuim</li>
-                <li>Lorem ipsuim</li>
-                <li>Lorem ipsuim</li>
-            </ul>
-        </div>
-    </div>
+    <?php //sidebar comes here
+    get_sidebar(  );
+    ?>
 </div>
 </div>
 
---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<div class="col starts-at-full ends-at-one-third clr box">
+    <div class="heading-holding-banner">
+        <h2>
+                <span>
+                    <span>
+                        You may also be interested in
+                    </span>
+                </span>
+        </h2>
+    </div>
+    <div class="breather">
+        <ul class="sibling">
+            <li>Lorem ipsuim</li>
+            <li>Lorem ipsuim</li>
+            <li>Lorem ipsuim</li>
+        </ul>
+    </div>
+</div>
